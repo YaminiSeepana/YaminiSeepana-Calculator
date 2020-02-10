ec1b5406166792ffbf88d0b6cb4ae99b0c22f37e
YaminiSeepana <177r1a05a6@gmail.com>
2020-02-10 21:53:43 +0530

    calci

---
+++ calculator/src/calculator/Calculate.java
@@ -0,0 +1,21 @@
+package calculator;
+
+
+public class Calculate {
+	public int Add(int a,int b)
+	{
+	return a+b;
+	}
+	public int sub(int a,int b)
+	{
+	return a-b;
+	}
+	public int mul(int a,int b)
+	{
+	return a*b;
+	}
+	public float div(float a,float b)
+	{
+	return a/b;
+	}
+}
